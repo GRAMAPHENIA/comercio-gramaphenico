4755ee36d0ce4db03a0be8382ae297f3e71fbc4a
gramaphenia <dicoratojuanpablo@gmail.com>
2024-06-04 17:53:26 -0300

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TITULOS</h1>
+      <h1>Suministros</h1>
     </main>
   );
 }
