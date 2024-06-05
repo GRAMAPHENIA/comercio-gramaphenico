a62bb108ec89c38e8844bcfdd587fb261500c512
gramaphenia <dicoratojuanpablo@gmail.com>
2024-06-05 11:09:10 -0300

    add-code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,36 @@
+import Post from "./components/Post";
 import styles from "./page.module.css";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+async function getPosts() {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+  // console.log({ posts });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Suministros</h1>
+    <main className={styles.main}>
+      <h1>Feed</h1>
+      {posts.map((post) => {
+        return (
+          <Post 
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          content={post.content}
+          authorName={post.author.name}
+          />
+        );
+      })}
     </main>
   );
 }
