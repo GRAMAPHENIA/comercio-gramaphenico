ba11693650420534b7867a856dfc424e0acfc6ce
gramaphenia <dicoratojuanpablo@gmail.com>
2024-06-08 14:26:30 -0300

    final-for-now

---
+++ src/app/api/post/[id]/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function DELETE(request, { params }) {
+  const id = params.id;
+
+  const post = await prisma.post.delete({
+    where: { id },
+  });
+
+  //   console.log({ id });
+  return NextResponse.json(post);
+}
--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -1,11 +1,14 @@
-export default function Post({id, title, content, authorName}) {
-    return (
-        <>
-        <div className="border border-white p-4 m-3">
-            <h3>{authorName}</h3>
-            <h4>{title}</h4>
-            <p>{content}</p>
-        </div>
-        </>
-    )
+import DeletePostButton from "./DeletePostButton";
+
+export default function Post({ id, title, content, authorName }) {
+  return (
+    <>
+      <div className="border border-white p-4 m-3">
+        <h3>{authorName}</h3>
+        <h4>{title}</h4>
+        <p>{content}</p>
+        <DeletePostButton postId={id} />
+      </div>
+    </>
+  );
 }
